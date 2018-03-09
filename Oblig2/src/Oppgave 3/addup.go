7262e38891c0ab1904bd635072ae7f3c7d7dac98
knuthenaes <35766206+knuthenaes@users.noreply.github.com>
2018-03-09 15:37:41 +0100

    Add files via upload

---
+++ Oblig2/src/Oppgave 3/addup.go
@@ -0,0 +1,69 @@
+package main
+
+
+import (
+	"fmt"
+	"time"
+	"os"
+	"os/signal"
+	"strconv"
+)
+
+func main() {
+
+	d := make(chan os.Signal, 1)
+	signal.Notify(d, os.Interrupt)
+
+	go func() {
+		<-d
+		fmt.Println("Code red: emergency shut down!")
+		os.Exit(1)
+	}()
+
+	c := make(chan int)
+	go readInput(c)
+	time.Sleep(5 * 1e9)
+	go addUp(c)
+	time.Sleep(5 * 1e9)
+}
+
+func readInput(c chan int) {
+
+	var n1 string
+	var n2 string
+
+	fmt.Println("Enter num: ")
+	fmt.Scan(&n1)
+
+	tall1, err := strconv.Atoi(n1)
+	if (err != nil) {
+		fmt.Println("Error, invalid input. Program shutting down ")
+		os.Exit(0)
+	}
+
+	fmt.Println("Enter num: ")
+	fmt.Scan(&n2)
+
+	tall2, err := strconv.Atoi(n2)
+	if (err != nil) {
+		fmt.Println("Error, invalid input. Program shutting down")
+		os.Exit(0)
+	}
+
+
+	c <- tall1 //sender data via channel
+	c <- tall2
+
+	res := <-c // mottar resultat fra channel
+	fmt.Println("Result: ", res)
+
+}
+
+func addUp(c chan int) {
+
+	n1, n2 := <-c, <-c // mottar data fra readInput()
+	res := (n1 + n2)
+
+	c <- res // sender resultat tilbake til readInput()
+
+}
